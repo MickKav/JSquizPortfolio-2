f616823910ed5435eb310c0a56fd1463b205250c
MickKav <>
2023-05-09 19:44:23 +0000

    Add variable for question

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: "What is the capital of Australia?",
+        answers: [
+            { text: "Perth", correct: false},
+            { text: "Sydney", correct: false},
+            { text: "Canberra", correct: true},
+            { text: "Melbourne", correct: false},
+        ]
+    },
+    {
+        question: "What is the capital of Brazil?",
+        answers: [
+            { text: "Rio de Janeiro", correct: false},
+            { text: "Brasilia", correct: true},
+            { text: "Sao Paolo", correct: false},
+            { text: "Recife", correct: false},
+        ]
+    },
+    {
+        question: "What is the capital of Switzerland?",
+        answers: [
+            { text: "Bern", correct: true},
+            { text: "Zurich", correct: false},
+            { text: "Geneva", correct: false},
+            { text: "Basel", correct: false},
+        ]
+    },
+    {
+        question: "What is the capital of Canada?",
+        answers: [
+            { text: "Vancouver", correct: false},
+            { text: "Toronto", correct: false},
+            { text: "Calgary", correct: false},
+            { text: "Ottawa", correct: true},
+        ]
+    },
+    {
+        question: "What is the capital Scotland?",
+        answers: [
+            { text: "Glasgow", correct: false},
+            { text: "Aberdeen", correct: false},
+            { text: "Edinburgh", correct: true},
+            { text: "Inverness", correct: false},
+        ]
+    },
+]
